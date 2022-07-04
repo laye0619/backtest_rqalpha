e37c0cec6c4dfb5af2d9ff4a4b3df8643c0069c7
laye0619 <laye0619@gmail.com>
2022-07-04 11:53:19 +0800

    new

--- backtest/bt_t28/00_bt_t28.py
+++ backtest/bt_t28/00_bt_t28.py
@@ -18,7 +18,7 @@ __config__ = {
         },
         # "data_1st-bundle-path": "/Users/i335644/.rqalpha/bundle",
         "start_date": "20130101",
-        "end_date": "20220601",
+        "end_date": "20220701",
     },
     "extra": {
         "log_level": "info",
