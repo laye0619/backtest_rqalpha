51b2441c7f62bdcfb53bb585abe2617f9da0c96d
LayeWang <laye0619@gmail.com>
2022-07-18 10:06:52 +0800

    change to 20, 25

--- backtest/rotation_strategy/bt/rs_me_ind/rs_me_ind_param_optimize.py
+++ backtest/rotation_strategy/bt/rs_me_ind/rs_me_ind_param_optimize.py
@@ -35,7 +35,7 @@ check_date_list = [
 
 tasks = []
 for check_date in check_date_list:
-    for sma_period in [30]:
+    for sma_period in [20, 25]:
         for trend_indicator_filter in range(1, 5, 1):
             for trend_indicator_buffer in range(0, 3, 1):
                 for holding_num in range(1, 4, 1):
